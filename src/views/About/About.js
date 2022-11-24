c53cac1ae0ef1ce567e0a8cc6702549d9a5a25a0
JagrutiMahajan04 <jagrutimaha2000@gmail.com>
2022-11-24 16:47:34 +0530

    view and components

---
+++ src/views/About/About.js
@@ -0,0 +1,16 @@
+import "./About.css"
+
+import Footer from "./../../components/Footer/Footer"
+
+function About(){
+    return (
+       <div className ="about">
+        <h1>About Page</h1>
+        <Footer/>
+
+       </div>
+       
+    )   
+}
+
+export default About
---
+++ src/views/Home/Home.js
@@ -0,0 +1,14 @@
+import "./Home.css"
+
+import Footer from "./../../components/Footer/Footer"
+
+function Home(){
+    return (
+       <div className ="home">
+        <h1>Home Page</h1>
+        <Footer/>
+        </div>
+    )   
+}
+
+export default Home
